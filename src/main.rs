c15b757983c9f494a45478be87a42c81015d5a0c
Mastyx <mastyx.house@gmail.com>
2026-05-06 12:59:01 +0200

    aggiunta oggetti prova
    cominciamo a capire primi oggetti inseriti
    vediamo di capire gli allineamenti

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use std::default;
 
-use eframe::egui;
+use eframe::{egui, wgpu::rwh::AppKitDisplayHandle};
 
 mod timer;
 //
@@ -36,28 +36,42 @@ mod timer;
 //    eframe::run_native("Pomodoro Widget", options, Box::new(|_cc| Box::new(app)))
 //}
 //
-//
 
-fn main() -> eframe::Result<()>{
-
-    // opzioni per la finestra 
-    let option = eframe::NativeOptions{
-        ..Default::default()
-    };
-    let applicazione = Application{};
-    eframe::run_native("Selettore Accessi rete ", option,  
-        Box::new(|_cc| Ok(Box::new(applicazione))))
+// struttura dell' applicazione
+fn main() {
+    let native_option = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Nome del Applicazione",
+        native_option,
+        Box::new(|cc| Ok(Box::new(Application::new(cc)))),
+    );
 }
 
-
+// struttira della nostra applicazione
+#[derive(Default)]
 struct Application {
-    // non aggiungiamo nullla per il momento
+    //
 }
-
-impl eframe::App for Application {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        
+impl Application {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
     }
-} 
-
-
+}
+impl eframe::App for Application {
+    fn ui(&mut self, ui: &mut egui::Ui, frame: &mut eframe::Frame) {
+        let contenitore = ui.ctx();
+        egui::TopBottomPanel::top("Top panel")
+            .min_height(50.0)
+            .show(contenitore, |ui| {
+                // pulsante per la chiusura
+                if ui.button("Esci").clicked() {};
+            });
+        egui::SidePanel::left("SidePanel").show(contenitore, |ui| {
+            ui.label("Elemento 1");
+            ui.label("Elemento 2");
+        });
+        egui::CentralPanel::default().show(contenitore, |ui| {
+            ui.heading("Testo del central panel ");
+        });
+    }
+}
